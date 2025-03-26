d644c0367f236f39c85803529200e526f16f827a
Moro22 <pedroeduardomorocruz@gmail.com>
2025-03-25 23:02:47 -0600

    Conexión a Railway MySQL configurada

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root", // Cambia si usas otro usuario
-    password: "1234321", // Si tienes contraseña, ponla aquí
-    database: "contrato_digital"
+    host: "ballast.proxy.rlwy.net",
+    port: 20038,
+    user: "root",
+    password: "CVwyAcKVWZnYSibVGiNfLgyStTPvclmr",
+    database: "railway"
 });
 
 db.connect((err) => {
     if (err) {
-        console.error("❌ Error conectando a MySQL:", err);
+        console.error("❌ Error conectando a Railway MySQL:", err);
     } else {
-        console.log("✅ Conectado a MySQL correctamente");
+        console.log("✅ Conectado a Railway MySQL correctamente");
     }
 });
 
