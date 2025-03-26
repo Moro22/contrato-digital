30c509350613a7ca4fd4010ce933d9198ca8c42a
Moro22 <pedroeduardomorocruz@gmail.com>
2025-03-25 22:20:55 -0600

    Actualizado para usar puerto dinámico en producción

--- backend/server.js
+++ backend/server.js
@@ -201,6 +201,8 @@ app.post("/enviar-contrato", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-    console.log(" Servidor corriendo en el puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
